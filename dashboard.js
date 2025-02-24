b15d722a3374f02a99ab8e79cf9762f704eb2088
Amandyq <amandykamandyk@yandex.kz>
2025-02-24 15:16:55 +0600

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -5,11 +5,20 @@ window.onload = function () {
 
     if (!userID) {
         alert("Алдымен жүйеге кіріңіз!");
-        window.location.href = "index.html"; // Егер пайдаланушы кірмеген болса, басты бетке қайтарады
+        window.location.href = "index.html";
     }
 
     document.getElementById("userName").innerText = userID;
     document.getElementById("userRole").innerText = role;
+
+    // Әр рөлге өз панелін көрсету
+    if (role === "student") {
+        document.getElementById("studentPanel").style.display = "block";
+    } else if (role === "teacher") {
+        document.getElementById("teacherPanel").style.display = "block";
+    } else if (role === "admin") {
+        document.getElementById("adminPanel").style.display = "block";
+    }
 }
 
 // Шығу функциясы
